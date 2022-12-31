a4824863b9b38f95cbdae545259afea42a9a7f26
Brendon Bown <brendon.bown@gmail.com>
2022-12-31 15:11:45 -0700

    Lex identifiers and keywords

--- lib/Lexer.ml
+++ lib/Lexer.ml
@@ -58,8 +58,7 @@ class lexer_state ?(curr_offset = 0) source =
     method curr_offset = curr_offset
 
     method peek_ahead n_chars =
-      if not (n_chars >= 0) then invalid_arg "n_chars (peek_ahead)"
-      else if not (self#is_in_bounds n_chars) then None
+      if not (self#is_in_bounds n_chars) then None
       else Some (String.get source (curr_offset + n_chars))
 
     method peek = self#peek_ahead 0
@@ -70,8 +69,7 @@ class lexer_state ?(curr_offset = 0) source =
       else String.sub source curr_offset length
 
     method advance n_chars =
-      if not (n_chars >= 0) then invalid_arg "n_chars (advance)"
-      else new lexer_state ~curr_offset:(curr_offset + n_chars) source
+      new lexer_state ~curr_offset:(curr_offset + n_chars) source
 
     method is_in_bounds n_chars = curr_offset + n_chars < String.length source
   end
@@ -80,15 +78,41 @@ let consume_token t_type length lex_state =
   ( Token.make t_type (lex_state#get_lexeme length) lex_state#curr_offset,
     lex_state#advance length )
 
-let rec find_string_end ?(length = 1) lex_state =
-  match lex_state#peek_ahead length with
-  | Some '\'' -> length + 1
-  | Some _ -> find_string_end ~length:(length + 1) lex_state
+let rec find_string_length ?(curr_char = 1) lex_state =
+  match lex_state#peek_ahead curr_char with
+  | Some '\'' -> curr_char + 1
+  | Some _ -> find_string_length ~curr_char:(curr_char + 1) lex_state
   | None -> failwith "Unimplemented string EOF handling"
 
 let consume_string lex_state =
-  if not (lex_state#peek = Some '\'') then invalid_arg "lex_state (consume_string)"
-  else let string_length = find_string_end lex_state in consume_token Token.String string_length lex_state
+  let string_length = find_string_length lex_state in
+  consume_token Token.String string_length lex_state
+
+let is_alpha input_char =
+  let input_char_code = Char.code input_char in
+  (input_char_code >= (Char.code 'A') && input_char_code <= (Char.code 'Z')) ||
+  (input_char_code >= (Char.code 'a') && input_char_code <= (Char.code 'z'))
+
+let is_numeric input_char =
+  let input_char_code = Char.code input_char in
+  input_char_code >= (Char.code '0') && input_char_code <= (Char.code '9')
+
+let is_alphanumeric input_char = is_alpha input_char || is_numeric input_char
+
+let rec find_ident_length ?(curr_char = 1) lex_state =
+  match lex_state#peek_ahead curr_char with
+  | Some c when is_alphanumeric c -> find_ident_length ~curr_char:(curr_char + 1) lex_state
+  | _ -> curr_char
+
+let consume_identifier lex_state =
+  let ident_length = find_ident_length lex_state in
+  let t_type = (match lex_state#get_lexeme ident_length with
+    | "Schemes" -> Token.Schemes
+    | "Facts" -> Token.Facts
+    | "Rules" -> Token.Rules
+    | "Queries" -> Token.Queries
+    | _ -> Token.Id) in
+  consume_token t_type ident_length lex_state
 
 let lex_token lex_state =
   match lex_state#peek with
@@ -104,6 +128,7 @@ let lex_token lex_state =
   | Some '*' -> consume_token Token.Multiply 1 lex_state
   | Some '+' -> consume_token Token.Add 1 lex_state
   | Some '\'' -> consume_string lex_state
+  | Some c when is_alpha c -> consume_identifier lex_state
   | Some _ -> consume_token Token.Undefined 1 lex_state
   | None -> consume_token Token.Eof 0 lex_state
 
